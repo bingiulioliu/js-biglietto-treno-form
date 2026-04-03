b2cd9bcdeeeba17d3974437838a8e7db2f90468b
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-03 16:12:15 +0200

    ADD collegamenti variabili-id

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Variabili per il form
+const myForm = document.querySelector('#my-form');
+const nomeCognome = document.querySelector('#nomeCognome');
+const anniPasseggero = document.querySelector('#anniPasseggero');
+const percorrenzaViaggio = document.querySelector('#percorrenza');
+const mySubmit = document.querySelector('#mySubmit');
+// Variabili per la card di riepilogo acquisto
+const cardGrazie = document.querySelector('#my-card');
+const graziePer = document.querySelector('#cardGrazie');
+const cardSconto = document.querySelector('#cardSconto');
+const cardPercorrenza = document.querySelector('#cardPercorrenza');
+
+// event.preventDefault(); per togliere il comportamento di defaul di refresh al submit
+
+
+
